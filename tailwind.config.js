578f4919c3c39222b88147ec02fc24282cda81e3
Patrick Maciel <patrickmaciel.info@gmail.com>
2022-09-08 22:30:10 -0300

    livewire and alpine installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,9 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
